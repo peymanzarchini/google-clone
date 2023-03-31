7968ea6b778d9067419ffb7ccdb47f1f6443b5cd
Peyman Zarchini <peymanzarchini@outlook.com>
2023-04-01 02:01:31 +0330

    Complete the SearchOptions component

--- src/components/header/SearchOptions.js
+++ src/components/header/SearchOptions.js
@@ -1,8 +1,36 @@
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { AiOutlineCamera, AiOutlineSearch } from "react-icons/ai";
+
 function SearchOptions() {
+  const pathName = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const term = searchParams.get("searchTerm");
+
+  function selectTab(tab) {
+    router.push(`/search/${tab === "Images" ? "image" : "web"}?searchTerm=${term}`);
+  }
+
   return (
-    <>
-      <h1>Search Options</h1>
-    </>
+    <div className="flex space-x-2 select-none border-b w-full justify-center lg:justify-start lg:pl-52 text-gray-700 text-sm">
+      <div
+        onClick={() => selectTab("All")}
+        className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
+          pathName === "/search/web" && "!text-blue-600 !border-blue-600"
+        }`}
+      >
+        <p>All</p>
+      </div>
+      <div
+        onClick={() => selectTab("Images")}
+        className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
+          pathName === "/search/image" && "!text-blue-600 !border-blue-600"
+        }`}
+      >
+        <p>Images</p>
+      </div>
+    </div>
   );
 }
 
