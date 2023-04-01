87e7c7d41c6cbb0289458c991363a2cc4cbe3293
Peyman Zarchini <peymanzarchini@outlook.com>
2023-04-01 05:17:08 +0330

    fetch data from google search api

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,9 +1,10 @@
-function WebSearch() {
-  return (
-    <>
-      <h1>Web Search</h1>
-    </>
+async function WebSearch({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
+  const data = await response.json();
+  const results = data.items;
+  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
 
 export default WebSearch;
