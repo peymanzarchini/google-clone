0ba22d479cd68f959616c4e012d51e3c8f8c8172
Peyman Zarchini <peymanzarchini@outlook.com>
2023-04-01 21:30:55 +0330

    Add ImageResult component

--- src/app/search/image/page.js
+++ src/app/search/image/page.js
@@ -1,9 +1,34 @@
-function ImageSearch() {
-  return (
-    <>
-      <h1>ImageSearch</h1>
-    </>
+import ImageSearchResult from "@/components/main/ImageSearchResult";
+import Link from "next/link";
+
+async function ImageSearch({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}&searchType=image`
   );
+
+  if (!response.ok) {
+    console.log(response);
+    throw new Error("Something went wrong");
+  }
+
+  const data = await response.json();
+
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+  return <>{results && <ImageSearchResult results={data} />}</>;
 }
 
 export default ImageSearch;
