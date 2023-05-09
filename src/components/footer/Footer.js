ef098b3a6355a19089012dc3bc870e97f41ec414
Peyman Zarchini <peymanzarchini@outlook.com>
2023-05-09 22:11:30 +0330

    update ui

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,8 +2,8 @@ import ShowCountry from "./ShowCountry";
 
 function Footer() {
   return (
-    <footer className="absolute bottom-0 text-sm text-gray-500 bg-[#f2f2f2] w-full">
-      <div className="border-b px-8 py-3">
+    <footer className="absolute bottom-0 text-sm text-gray-500 bg-[#f2f2f2] w-full h-8">
+      <div className="border-b px-8 py-1 h-[100%]">
         <ShowCountry />
       </div>
       <div className="flex flex-col sm:flex-row justify-between items-center px-8 py-3 space-y-7 sm:space-y-0">
